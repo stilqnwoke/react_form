bf1b044893a73fa76bf8020cabce0ff4261c4ea4
stilqnwoke <69080350+stilqnwoke@users.noreply.github.com>
2023-01-12 12:18:43 +0200

    Added validation messages

---
+++ react_form/src/utils/validateForm.jsx
@@ -0,0 +1,73 @@
+const EMAIL_REGEX = /\S+@\S+\.\S+/;
+const PHONE_NUMBER_REGEX = /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
+const URL_REGEX = /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/gm;
+
+export default function validateForm(values) {
+  let errors = {};
+
+  if (!values.email) {
+    errors.email = "Email is required";
+  } else if (!EMAIL_REGEX.test(values.email)) {
+    errors.email =
+      "Email address is invalid. Please make sure there is a '@' symbol";
+  }
+
+  if (!values.password) {
+    errors.password = "Password is required";
+  } else if (values.password.length < 10) {
+    errors.password = "Password needs to be more than 10 characters";
+  }
+
+  if (!values.number) {
+    errors.number = "Number is required";
+  }
+
+  if (!values.tel) {
+    errors.tel = "Phone number is required";
+  } else if (!PHONE_NUMBER_REGEX.test(values.tel)) {
+    errors.tel = "Phone number is invalid";
+  }
+
+  if (!values.text) {
+    errors.text = "Text is required";
+  } else if (values.text.length < 3) {
+    errors.tel = "Text needs to be more than 3 characters";
+  }
+
+  if (!values.url) {
+    errors.url = "URL is required";
+  } else if (!URL_REGEX.test(values.url)) {
+    errors.url =
+      "URL must be valid.Make sure the string contains http followed by ://";
+  }
+
+  if (!values.textarea) {
+    errors.textarea = "Textarea is required";
+  }
+
+  if (!values.date) {
+    errors.date = "Date is required";
+  }
+
+  if (!values.datetimeLocal) {
+    errors.datetimeLocal = "DatetimeLocal is required";
+  }
+
+  if (!values.month) {
+    errors.month = "Month is required";
+  }
+
+  if (!values.time) {
+    errors.time = "Time is required";
+  }
+
+  if (!values.week) {
+    errors.week = "Week is required";
+  }
+
+  if (!values.file) {
+    errors.file = "File is required";
+  }
+
+  return errors;
+}
